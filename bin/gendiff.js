17a8ee37f0b071caf0c0f2f94c2f343bc37f010f
Bogdan <homoretsky@gmail.com>
2022-07-26 21:50:24 +0500

    Update gendiff.js & add -f option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,9 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
+  // .argument('<type>', 'output format')
+  .argument('<filepath1>', 'filepath first')
+  .argument('<filepath2>', 'filepath second');
 
 program.parse();
