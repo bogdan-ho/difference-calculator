10583189cb8a5069d5de1764afc6b230eeecee21
Bogdan <homoretsky@gmail.com>
2022-07-27 17:34:51 +0500

    Add genDiff() function

---
+++ src/genDiff.js
@@ -0,0 +1,70 @@
+import _ from "lodash";
+import path from "node:path";
+import fs from "node:fs";
+
+const genDiff = (filepath1, filepath2) => {
+  const pathResolved1 = path.resolve(process.cwd(), filepath1);
+  const pathResolved2 = path.resolve(process.cwd(), filepath2);
+
+  const file1 = fs.readFileSync(pathResolved1);
+  const file2 = fs.readFileSync(pathResolved2);
+
+  const fileObj1 = JSON.parse(file1);
+  const fileObj2 = JSON.parse(file2);
+
+  const keys1 = Object.keys(fileObj1).sort();
+  const keys2 = Object.keys(fileObj2).sort();
+  const keys = _.uniq([...keys1, ...keys2]).sort();
+
+  // console.log(`pathResolved is ${pathResolved1}`);
+  // console.log(`file1 is ${file1}`);
+  // console.log(`keys1 is ${keys1}`);
+  // console.log(`keys2 is ${keys2}`);
+  // console.log(`file1Obj is ${fileObj1}`);
+
+  const diffedKeys = keys.map((key) => {
+    if (fileObj1[key] === fileObj2[key]) {
+      return { key, value: fileObj1[key], type: "unchanged" };
+    } else if (fileObj2[key] === undefined) {
+      return { key, value: fileObj1[key], type: "deleted" };
+    } else if (fileObj1[key] === undefined) {
+      return { key, value: fileObj2[key], type: "added" };
+    } else {
+      return { key, value1: fileObj1[key], value2: fileObj2[key], type: "changed"  };
+    }
+  });
+
+  // console.log(`diffedKeys is ${diffedKeys}`);
+  // console.log(`diffedKeys is ${JSON.stringify(diffedKeys[0])}`);
+  // console.log(`diffedKeys is ${JSON.stringify(diffedKeys[1])}`);
+  // console.log(`diffedKeys is ${JSON.stringify(diffedKeys[2])}`);
+
+  const diffedKeysStringify = diffedKeys
+    .map((obj) => {
+      switch (obj.type) {
+        case "unchanged":
+          return `    ${obj.key}: ${obj.value}`;
+        case "deleted":
+          return `  - ${obj.key}: ${obj.value}`;
+        case "added":
+          return `  + ${obj.key}: ${obj.value}`;
+        case "changed":
+          return [`  - ${obj.key}: ${obj.value1}`, `  + ${obj.key}: ${obj.value2}`];
+        default:
+          return console.error(`Передан неправильный оператор: ${obj.type}`);
+      }
+    })
+    .flat();
+  // console.log(`diffedKeysStringify is ${diffedKeysStringify}`);
+
+  const result = ['{', ...diffedKeysStringify, '}'].join('\n');
+
+  // console.log(`result is ---------------------`);
+  // console.log(`result is ${result}`);
+  // console.log(`typeof(result) is ${Array.isArray(result)}`);
+
+  return console.log(result);
+
+};
+
+export default genDiff;
