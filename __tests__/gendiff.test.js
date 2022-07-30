037b03796378e45a3b81f7fc5ced4037fb644625
Bogdan <homoretsky@gmail.com>
2022-07-30 12:06:28 +0500

    Add automatic tests

---
+++ __tests__/gendiff.test.js
@@ -0,0 +1,23 @@
+import genDiff from "../src/genDiff.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'node:path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+test('genDiff', () => {
+  const filepath1 = path.resolve(__dirname,'..' ,'__fixtures__/file1.json');
+  const filepath2 = path.resolve(__dirname,'..' ,'__fixtures__/file2.json');
+  const result = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
+
+  expect(genDiff(filepath1, filepath2)).toBe(result)
+});
