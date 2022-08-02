bc8ddaf7a39d08d47d828f1888b59c359e4c645d
Bogdan <homoretsky@gmail.com>
2022-08-02 19:53:33 +0500

    Update genDiff.js

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import path from 'node:path';
 import getFileObject from './parsers.js';
+import stylish from './stylish.js';
 
 const genDiff = (filepath1, filepath2) => {
   const pathResolved1 = path.resolve(process.cwd(), filepath1);
@@ -9,47 +10,38 @@ const genDiff = (filepath1, filepath2) => {
   const fileObj1 = getFileObject(pathResolved1);
   const fileObj2 = getFileObject(pathResolved2);
 
-  const keys1 = Object.keys(fileObj1).sort();
-  const keys2 = Object.keys(fileObj2).sort();
-  const keys = _.uniq([...keys1, ...keys2]).sort();
+  const iter = (object1, object2) => {
+    const keys1 = Object.keys(object1).sort();
+    const keys2 = Object.keys(object2).sort();
+    const keysAll = _.uniq([...keys1, ...keys2]).sort();
 
-  const diffedKeys = keys.map((key) => {
-    if (fileObj1[key] === fileObj2[key]) {
-      return { key, value: fileObj1[key], type: 'unchanged' };
-    }
-    if (fileObj2[key] === undefined) {
-      return { key, value: fileObj1[key], type: 'deleted' };
-    }
-    if (fileObj1[key] === undefined) {
-      return { key, value: fileObj2[key], type: 'added' };
-    }
-    return {
-      key, value1: fileObj1[key], value2: fileObj2[key], type: 'changed',
-    };
-  });
-
-  const diffedKeysStringify = diffedKeys
-    .map((obj) => {
-      switch (obj.type) {
-        case 'unchanged':
-          return `    ${obj.key}: ${obj.value}`;
-        case 'deleted':
-          return `  - ${obj.key}: ${obj.value}`;
-        case 'added':
-          return `  + ${obj.key}: ${obj.value}`;
-        case 'changed':
-          return [`  - ${obj.key}: ${obj.value1}`, `  + ${obj.key}: ${obj.value2}`];
-        default:
-          return console.error(`Передан неправильный оператор: ${obj.type}`);
+    const diffedKeys = keysAll.map((key) => {
+      if (_.isObject(object1[key]) && _.isObject(object2[key])) {
+        return { key, value: iter(object1[key], object2[key]), type: 'nested' };
       }
-    })
-    .flat();
+      if (object1[key] === object2[key]) {
+        return { key, value: object1[key], type: 'unchanged' };
+      }
+      if (object2[key] === undefined) {
+        return { key, value: object1[key], type: 'deleted' };
+      }
+      if (object1[key] === undefined) {
+        return { key, value: object2[key], type: 'added' };
+      }
+      return {
+        key, value1: object1[key], value2: object2[key], type: 'changed',
+      };
+    });
 
-  const result = ['{', ...diffedKeysStringify, '}'].join('\n');
+    return diffedKeys;
+  };
 
-  console.log(result);
+  const result = iter(fileObj1, fileObj2);
+  const finalResult = stylish(result);
 
-  return result;
+  console.log(finalResult);
+
+  return finalResult;
 };
 
 export default genDiff;
