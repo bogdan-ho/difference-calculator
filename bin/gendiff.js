88596e58125d9312479326d4c3192086ca0f899e
Bogdan <homoretsky@gmail.com>
2022-08-09 16:44:39 +0500

    Update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -9,9 +9,11 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0', '-V, --version', 'output the version number')
-  .option('-f, --format <type>', 'stylish')
+  .option('-f, --format <type>', 'output format (stylish, plain, json)', 'stylish')
   .argument('<filepath1>', 'filepath first')
   .argument('<filepath2>', 'filepath second')
-  .action(genDiff);
+  .action((filepath1, filepath2) => {
+    genDiff(filepath1, filepath2, program.opts().format);
+  });
 
 program.parse();
