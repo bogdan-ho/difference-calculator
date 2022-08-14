de12152558ecd96eb1200d7280e29b7bcf765c7e
Bogdan <homoretsky@gmail.com>
2022-08-14 16:06:05 +0500

    Update parser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import path from 'node:path';
-import getFileObject from './parsers.js';
+import fs from 'node:fs';
+import yaml from 'js-yaml';
+import getFileExtension from './parsers.js';
 import makeFormatted from './formatters/index.js';
 import buildTree from './buildTree.js';
 
 const getPathResolved = (filepath) => path.resolve(process.cwd(), filepath);
+
+const getJsonFile = (filepath) => JSON.parse(fs.readFileSync(filepath));
+const getYamlFile = (filepath) => yaml.load(fs.readFileSync(filepath));
+
+const getFileObject = (filepathResolved) => {
+  const ext = getFileExtension(filepathResolved);
+
+  switch (ext) {
+    case ('json'):
+      return getJsonFile(filepathResolved);
+    case ('yml'): case ('yaml'): // case ('yml' || 'yaml'):
+      return getYamlFile(filepathResolved);
+    default:
+      throw new Error(`Передан неправильное расширение файла ext: ${ext}`);
+  }
+};
 
 const genDiff = (filepath1, filepath2, formatName = 'stylish') => {
   const pathResolved1 = getPathResolved(filepath1);
--- src/parsers.js
+++ src/parsers.js
@@ -1,5 +1,3 @@
-import fs from 'node:fs';
-import yaml from 'js-yaml';
 import path from 'node:path';
 
 const getFileExtension = (filepathResolved) => {
@@ -7,20 +5,4 @@ const getFileExtension = (filepathResolved) => {
   return ext.slice(1);
 };
 
-const getJsonFile = (filepath) => JSON.parse(fs.readFileSync(filepath));
-const getYamlFile = (filepath) => yaml.load(fs.readFileSync(filepath));
-
-const getFileObject = (filepathResolved) => {
-  const ext = getFileExtension(filepathResolved);
-
-  switch (ext) {
-    case ('json'):
-      return getJsonFile(filepathResolved);
-    case ('yml'): case ('yaml'): // case ('yml' || 'yaml'):
-      return getYamlFile(filepathResolved);
-    default:
-      throw new Error(`Передан неправильное расширение файла ext: ${ext}`);
-  }
-};
-
-export default getFileObject;
+export default getFileExtension;
